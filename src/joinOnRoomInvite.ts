66644c0c0be6997e9735a5568d0f16044d42045a
woeishi <woei@refect.net>
2023-06-30 01:40:22 +0200

    set m.direct account data for DM rooms

--- src/joinOnRoomInvite.ts
+++ src/joinOnRoomInvite.ts
@@ -1,13 +1,30 @@
-import { MatrixClient, RoomMemberEvent } from "matrix-js-sdk";
+import { EventType, MatrixClient, RoomMemberEvent } from "matrix-js-sdk";
 
 export function joinOnRoomInvite(client: MatrixClient) {
   client.on(RoomMemberEvent.Membership, async (event, member) => {
-    if (member.membership === "invite" && member.userId === client.getUserId()) {
-      const { joined_rooms } = await client.getJoinedRooms();
-      if (!joined_rooms.includes(member.roomId)) {
-        client.joinRoom(member.roomId).then(() => {
-          console.log("Auto-joined %s", member.roomId);
-        });
+    if (member.membership === "invite") {
+      if (member.userId === client.getUserId()) {
+        const { joined_rooms } = await client.getJoinedRooms();
+        if (!joined_rooms.includes(member.roomId)) {
+          client.joinRoom(member.roomId).then(() => {
+            console.log("Auto-joined %s", member.roomId);
+          });
+        }
+      }
+      // m.direct account data must be set for DMs
+      // https://spec.matrix.org/v1.7/client-server-api/#client-behaviour-21
+      if (event.getContent().is_direct) {
+        let directEvent = client.getAccountData(EventType.Direct);
+        let directData = directEvent?.getContent() ?? {};
+        const otherId =
+          member.userId === client.getUserId()
+            ? event.getSender() ?? member.userId
+            : member.userId;
+        if (!directData[otherId]) {
+          directData[otherId] = member.roomId;
+          client.setAccountData(EventType.Direct, directData);
+          console.log(`room ${member.roomId} marked as direct with ${otherId}`);
+        }
       }
     }
   });
